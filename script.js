ed0b23495aa61ec6a2ee339cc6b7206fcc28d491
Enrico <enricocavaliere56@gmail.com>
2026-04-02 09:24:35 -0300

    carrinho
    consegui fazer o botão de abrir e fechar o carrinho, agr estou tentando fazer mudar a quantidade da compra

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-let carrinho = document.querySelector('carrinho');
-let close = document.querySelector('close');
+let carrinho = document.getElementById('carrinho');
+let close = document.getElementById('fechar');
 let body = document.querySelector('body');
+let cartab = document.getElementById('cartab');
+
 
 carrinho.addEventListener('click', () => {
-    body.classList.toggle('mostrar')
-})
+    cartab.classList.remove('ocultar');
+    cartab.classList.add('mostrar');
+});
 close.addEventListener('click', () => {
-     body.classList.toggle('mostrar')
-})
+    cartab.classList.remove('mostrar');
+    cartab.classList.add('ocultar');
+});
+
+const valor = document.getElementById ('valor');
+const maisButton = document.getElementById('mais');
+const menosButton = document.getElementById('menos');
+
+const updtadeValor = () => {
+    valor.innerHTML = quantidade;
+};
+let quantidade= 0;
+let intervalId = 0;
+maisButton.addEventListener('mousedown', () => {
+    intervalId = setInterval (() => {
+        quantidade += 1;
+        updtadeValor()
+    }, 100);
+});
